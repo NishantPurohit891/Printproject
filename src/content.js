ecf02e8f914627f4955aa6ab33db46fdfd44fa46
NishantPurohit891 <nishant.a.purohit@gmail.com>
2023-03-13 17:28:50 +0530

    first commit

---
+++ src/content.js
@@ -0,0 +1,223 @@
+const productData = [
+    {
+      id: 1,
+      name: "Writing paper",
+      image:
+        "https://images.ctfassets.net/zar1ypr5qpcx/5EbN5a3iwmZjDglTPGJrya/a0ecb128d2c7d9f123cd9606ae6a7540/Cairebag_featured_met_patroon.png?w=520&h=520&q=70&fm=webp&f=center",
+      productType: "openBackHeadphones",
+      price: 220,
+      rating: 5,
+      timeLeft: 27,
+      totalSales: 7479 
+    },
+    {
+      id: 2,
+      name: "Quotation and presentation folders",
+      image:
+        "https://images.ctfassets.net/zar1ypr5qpcx/4OCZdrmJKoPpcsqkDUXgrA/02804820ace028025cbc8155c616404e/Offertemap-Premium1-a4-featured.jpg?w=520&h=520&q=80&fm=webp&f=center",
+      productType: "openBackHeadphones",
+      price: 169,
+      rating: 5,
+      timeLeft: 24,
+      totalSales: 6898
+    },
+    {
+      id: 3,
+      name: "Memo pads (self-adhesive)",
+      image:
+        "https://images.ctfassets.net/zar1ypr5qpcx/4E6tqrKBCG2bIql5nwjjV0/3d6fcb7a06328fa29bb36235e0d58aa9/Memoblok-standaard_Featured.jpg?w=520&h=520&q=80&fm=webp&f=center",
+      productType: "openBackHeadphones",
+      price: 170,
+      rating: 4,
+      timeLeft: 21,
+      totalSales: 6347 
+    },
+    {
+        id: 4,
+        name: "Calendars",
+        image:
+          "https://images.ctfassets.net/zar1ypr5qpcx/7H1DWUJIasu3HXNiSwjJn8/c9499cb743b6cd7e64a37d6afb97f44c/Kalenders_featured2023.jpg?w=520&h=520&q=80&fm=webp&f=center",
+        productType: "openBackHeadphones",
+        price: 170,
+        rating: 4,
+        timeLeft: 21,
+        totalSales: 6347 
+      } ,
+      {
+        id: 5,
+        name: "ring binders",
+        image:
+          "https://images.ctfassets.net/zar1ypr5qpcx/tJ8Ef6tkmx6QMgGyoVjaH/a0bac695cc0473e58cf397007c6c94bf/Mappen_Featured.jpg?w=520&h=520&q=80&fm=webp&f=center",
+        productType: "openBackHeadphones",
+        price: 170,
+        rating: 4,
+        timeLeft: 21,
+        totalSales: 6347 
+      } ,
+      {
+        id: 6,
+        name: "Burnout Sets",
+        image:
+          "https://images.ctfassets.net/zar1ypr5qpcx/2Bgw0tGyXqh3rtjPyap4U5/ace43122fac8d8e611a74507989abe14/Doordruksets-a4_Featured.jpg?w=520&h=520&q=80&fm=webp&f=center",
+        productType: "openBackHeadphones",
+        price: 170,
+        rating: 4,
+        timeLeft: 21,
+        totalSales: 6347 
+      } ,
+      {
+        id: 7,
+        name: "Pantone and color fans",
+        image:
+          "https://images.ctfassets.net/zar1ypr5qpcx/LYsVMQycDJWbCYY5IEeDp/7e8672df5a3892c46ca7583c3c419e26/Solid-Guide-set.png?w=520&h=520&q=80&fm=webp&f=center",
+        productType: "openBackHeadphones",
+        price: 170,
+        rating: 4,
+        timeLeft: 21,
+        totalSales: 6347 
+      } ,
+      {
+        id:8,
+        name: "Notebooks",
+        image:
+          "https://images.ctfassets.net/zar1ypr5qpcx/5KxkH9RWRUcgaYUIUEcQ0C/1adbf959e40aef26b1f60d0696af0ae3/notitieboekjes-header-1-2000px.png?w=520&h=520&q=80&fm=webp&f=center",
+        productType: "openBackHeadphones",
+        price: 170,
+        rating: 4,
+        timeLeft: 21,
+        totalSales: 6347 
+      } ,
+      {
+        id: 9,
+        name: "Business cards in package",
+        image:
+          "https://images.ctfassets.net/zar1ypr5qpcx/3iM9yVnkwRUSqFsnu1zCpk/c32b091f0dac8d84bb5c83212c8c1a63/Visitekaartjes-Pakket_Featured.jpg?w=520&h=520&q=80&fm=webp&f=center",
+        productType: "openBackHeadphones",
+        price: 170,
+        rating: 4,
+        timeLeft: 21,
+        totalSales: 6347 
+      } ,
+      {
+        id: 10,
+        name: "Envelopes (printed)",
+        image:
+          "https://images.ctfassets.net/zar1ypr5qpcx/66wv6hgJf6dvr37wBBwob6/1765a0f5ce2ebfef543db629a13ef55c/stickersklein-featured.png?w=520&h=520&q=80&fm=webp&f=center",
+        productType: "openBackHeadphones",
+        price: 170,
+        rating: 4,
+        timeLeft: 21,
+        totalSales: 6347 
+      } ,
+      {
+        id:11,
+        name: "Stickers small size",
+        image:
+          "https://images.ctfassets.net/zar1ypr5qpcx/yM9msm8EMp0EaFYvDmlyY/ed23e23873d839eb9e5e47c079df71eb/Enveloppen-featured.jpg?w=520&h=520&q=80&fm=webp&f=center",
+        productType: "openBackHeadphones",
+        price: 45,
+        rating: 4,
+        timeLeft: 21,
+        totalSales: 6347 
+      }  ,
+      {
+        id: 12,
+        name: "large size stickers",
+        image:
+          "https://images.ctfassets.net/zar1ypr5qpcx/3BO9w6BMyOJu3xcidZtmHG/4571bba00faa703ea7b79c6a960e183c/stickersgrootformaat-featured.jpg?w=520&h=520&q=80&fm=webp&f=center",
+        productType: "openBackHeadphones",
+        price: 300,
+        rating: 2,
+        timeLeft: 21,
+        totalSales: 6347 
+      } ,
+      {
+        id: 13,
+        name: "dominant Stickers",
+        image:
+          "https://images.ctfassets.net/zar1ypr5qpcx/1W62RjdiKapneJmhtP4YX0/a6f9cac98bc05d18bece1c7b4e8bd45b/Doming_stickers_Featured.jpg?w=520&h=520&q=80&fm=webp&f=center",
+        productType: "openBackHeadphones",
+        price: 200,
+        rating: 3,
+        timeLeft: 21,
+        totalSales: 6347 
+      } ,
+      {
+        id: 14,
+        name: "Stickers on a roll",
+        image:
+          "https://images.ctfassets.net/zar1ypr5qpcx/11zzJdVz4TfGgW4ZW89YYX/3105859cac286f59c6f18326d8daad8e/Stickers-op-rol-featured_algemeen.jpg?w=520&h=520&q=80&fm=webp&f=center",
+        productType: "openBackHeadphones",
+        price: 110,
+        rating: 5,
+        timeLeft: 21,
+        totalSales: 6347 
+      } ,
+      {
+        id: 15,
+        name: "Lettering",
+        image:
+          "https://images.ctfassets.net/zar1ypr5qpcx/6WunZJdRLw1291HiHdJetF/0364e04e39c33c9a0e0cc16ad57c5fbc/belettering-featured.jpg?w=520&h=520&q=80&fm=webp&f=center",
+        productType: "openBackHeadphones",
+        price: 88,
+        rating: 4,
+        timeLeft: 21,
+        totalSales: 6347 
+      } ,
+      {
+        id: 16,
+        name: "Window stickers",
+        image:
+          "https://images.ctfassets.net/zar1ypr5qpcx/7bseuwCWzMMq8Gb4BcvlIX/720c6c50a473e12f3d75acf8796e36d2/raamsticker-featured.jpg?w=520&h=520&q=80&fm=webp&f=center",
+        productType: "openBackHeadphones",
+        price: 65,
+        rating: 1,
+        timeLeft: 21,
+        totalSales: 6347 
+      } ,
+       {
+        id: 17,
+        name: "wall stickers",
+        image:
+          "https://images.ctfassets.net/zar1ypr5qpcx/1H5QHXtwzeuocIiUeC0KUk/c52e810d3fdc9d9710494a0a6afff587/Muurstickers-2.jpg?w=520&h=520&q=80&fm=webp&f=center",
+        productType: "openBackHeadphones",
+        price: 185,
+        rating: 4,
+        timeLeft: 21,
+        totalSales: 6347 
+      } ,
+      {
+        id:18,
+        name: "floor stickers",
+        image:
+          "https://images.ctfassets.net/zar1ypr5qpcx/1MOrnM6UEikxtOPdSRSIxv/0fb1dc5f4d3896e30e822dc5c53910d3/vloersticker-featured.jpg?w=520&h=520&q=80&fm=webp&f=center",
+        productType: "openBackHeadphones",
+        price: 200,
+        rating: 3,
+        timeLeft: 21,
+        totalSales: 6347 
+      } ,
+      {
+        id:19,
+        name: "Packing Labels",
+        image:
+          "https://images.ctfassets.net/zar1ypr5qpcx/1jvLGb9OXBWvF9519DwS7w/da8954a18b6509320b95ba1225c63b60/Stickersoprolvoedsel4.jpg?w=520&h=520&q=80&fm=webp&f=center",
+        productType: "openBackHeadphones",
+        price: 43,
+        rating: 4,
+        timeLeft: 21,
+        totalSales: 6347 
+      } ,
+      {
+        id:20,
+        name: "Printed tape",
+        image:
+          "https://images.ctfassets.net/zar1ypr5qpcx/1mNZZvRVPiOceUWc2YaWYs/33791f94a6a56b664b291552ee971fc7/bedrukte-tape-bestellen.png?w=520&h=520&q=80&fm=webp&f=center",
+        productType: "openBackHeadphones",
+        price: 180,
+        rating: 4,
+        timeLeft: 21,
+        totalSales: 6347 
+      } 
+  ];
+  export default productData;
